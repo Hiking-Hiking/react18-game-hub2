ebf44396604d0d0726266df3e0f8dc7e8c7a07f7
Hiking-Hiking <13631647579@163.com>
2024-10-17 12:30:00 +0800

    Refactoring-fetch games with React Query

---
+++ src/services/api-client.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
+export default axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "de48b65d21ed495596bfee8499b3bd0c",
+  },
+});
