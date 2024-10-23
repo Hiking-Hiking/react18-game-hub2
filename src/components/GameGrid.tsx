2e2d1278f0b5ecb33ba41b0fdbf702061e72d34d
Hiking-Hiking <13631647579@163.com>
2024-10-23 10:57:34 +0800

    refactor(react query useInfiniteQuery): 使用react-query的useInfiniteQuery实现无限加载功能
    - 在useGames.ts钩子函数里，使用useInfiniteQuery替换掉useQuery，实现无限分页加载数据功能；
     - 在GameGrid.tsx组件里，修改ui，重构数据渲染，新增加载更多按钮功能。

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,5 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Button, SimpleGrid, Text } from "@chakra-ui/react";
+import React from "react";
 import { GameQuery } from "../App";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
@@ -10,29 +11,47 @@ interface Props {
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data, error, isLoading } = useGames(gameQuery);
+  const {
+    data,
+    error,
+    isLoading,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
+
+  console.log("游戏获取到的data", data);
 
   if (error) return <Text>{error.message}</Text>;
 
   return (
-    <SimpleGrid
-      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-      padding="10px"
-      spacing={6}
-    >
-      {isLoading &&
-        skeletons.map((skeleton) => (
-          <GameCardContainer key={skeleton}>
-            <GameCardSkeleton />
-          </GameCardContainer>
+    <Box padding="10px">
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
+        {data?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.results.map((game) => (
+              <GameCardContainer key={game.id}>
+                <GameCard game={game} />
+              </GameCardContainer>
+            ))}
+          </React.Fragment>
         ))}
-      {data?.results.map((game) => (
-        <GameCardContainer key={game.id}>
-          <GameCard game={game} />
-        </GameCardContainer>
-      ))}
-    </SimpleGrid>
+      </SimpleGrid>
+      {/* 添加按钮，动态显示按钮和按钮中的文字 */}
+      {hasNextPage && (
+        //添加点击事件，直接调用fetchNextPage()获取下一页数据
+        <Button onClick={() => fetchNextPage()} marginY={5}>
+          {isFetchingNextPage ? "Loading..." : "Load more"}
+        </Button>
+      )}
+    </Box>
   );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
@@ -13,19 +13,30 @@ export interface Game {
 }
 //实例化APIClient类，传入endpoint参数'/genres';
 const apiClient = new APIClient<Game>("/games");
+
 const useGames = (gameQuery: GameQuery) =>
-  useQuery<FetchResponse<Game>, Error>({
+  useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
     //queryFn是需要引用apiClient.getAll方法，不是调用它的结果；使用箭头函数，函数里返回对apiClient.getAll的引用，并给apiClient.getAll传入筛选参数；
-    queryFn: () => {
+
+    // pageParam 初始值设为1，这样获取了第一页数据；
+    queryFn: ({ pageParam = 1 }) => {
       return apiClient.getAll({
         params: {
           genres: gameQuery.genre?.id,
           parent_platforms: gameQuery.platform?.id,
           ordering: gameQuery.sortOrder,
           search: gameQuery.searchText,
+          //将pageParam赋给请求参数page
+          page: pageParam,
         },
       });
+    },
+    // react-query会调用这个函数，计算下一页页码；
+    getNextPageParam: (lastPage, allPages) => {
+      // console.log("lastPage", lastPage,"allPages", allPages);
+      // allPages包含我们当前检索到的每个页面的数据；所以直接用它的长度加1；
+      return lastPage.next ? allPages.length + 1 : undefined;
     },
   });
 
